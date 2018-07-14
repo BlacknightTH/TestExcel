2b204e23ca08e668a96a851be41f6c7145e0a1c8
BlacknightTH <palmkung4@gmail.com>
2018-07-14 21:01:12 +0700

    20(fail)

--- TestExcel/Controllers/TimeScheduleController.cs
+++ TestExcel/Controllers/TimeScheduleController.cs
@@ -62,12 +62,12 @@ namespace TestExcel.Controllers
                             SECTION_DATE = e1.SECTION_DATE,
                             SECTION_TEACHER = e1.SECTION_TEACHER,
                             SECTION_TIME_START = e1.SECTION_TIME_START,
-                            SECTION_TIME_END = e1.SECTION_TIME_END
+                            SECTION_TIME_END = e1.SECTION_TIME_END,
+                            SECTION_PERIOD = e1.SECTION_TIME_END - e1.SECTION_TIME_START
                         };
             ViewBag.FacultyName = Faculty_Name;
             ViewBag.ddl_Faculty = new SelectList(db.FACULTies.ToList(), "ID", "FACULTY_NAME");
             ViewBag.DDLSelected = Faculty_id;
-                var rr = query.Where(x => x.SECTION_TIME_START <= 15.00 && x.SECTION_DATE == "M").Any();
                 //query = query.Where(x => x.SECTION_NUMBER != "");
                 return View(query);
         }
