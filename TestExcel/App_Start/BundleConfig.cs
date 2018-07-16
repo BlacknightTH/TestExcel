305192d3e0b9ed890d072c6abe614b93d5c90e8e
BlacknightTH <palmkung4@gmail.com>
2018-07-16 14:26:17 +0700

    23

--- TestExcel/App_Start/BundleConfig.cs
+++ TestExcel/App_Start/BundleConfig.cs
@@ -9,9 +9,10 @@ namespace TestExcel
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-3.3.1.min.js",
+                        "~/Scripts/jquery-ui-1.12.1.min.js",
                         "~/Scripts/jquery.unobtrusive-ajax.min.js",
-                        "~/Scripts/DataTables/jquery.dataTables.js"));
+                        "~/Scripts/DataTables/jquery.dataTables.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,12 +27,13 @@ namespace TestExcel
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/Style.css",
                       "~/Content/site.css",
                       "~/Content/w3.css",
                       "~/Content/font-awesome.min.css",
-                      "~/Content/themes/base/jquery-ui.css",
-                      "~/Content/DataTables/css/jquery.dataTables.css"));
+                      "~/Content/themes/base/jquery-ui.min.css",
+                      "~/Content/DataTables/css/jquery.dataTables.min.css"));
         }
     }
 }
