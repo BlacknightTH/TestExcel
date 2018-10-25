f24e8ae1e4d9b42a7c8a368a351abf80f327bb18
BlacknightTH <palmkung4@gmail.com>
2018-10-25 18:47:40 +0700

    25/10/2561-2

---
+++ TestExcel/Controllers/DataController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestExcel.Data;
+using TestExcel.Models;
+using System.Text;
+using TestExcel.Utility;
+
+namespace TestExcel.Controllers
+{
+    public class DataController : Controller
+    {
+        TestExcelEntities db = new TestExcelEntities();
+        // GET: Data
+        public ActionResult Section()
+        {
+            var model = db.SECTIONs.ToList();
+            return View(model);
+        }
+        public ActionResult Subject()
+        {
+            var model = db.SUBJECTs.ToList();
+            return View(model);
+        }
+        public ActionResult Member()
+        {
+            var model = db.USERs.ToList();
+            return View(model);
+        }
+        public ActionResult Professor()
+        {
+            var model = db.PROFESSORs.ToList();
+            return View(model);
+        }
+        public ActionResult Department()
+        {
+            var model = db.DEPARTMENTs.ToList();
+            return View(model);
+        }
+        public ActionResult Building()
+        {
+            var model = db.BUILDINGs.ToList();
+            return View(model);
+        }
+        public ActionResult Branch()
+        {
+            var model = db.BRANCHes.ToList();
+            return View(model);
+        }
+    }
+}
